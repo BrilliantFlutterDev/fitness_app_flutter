e06873bfef47e76c15372396ec1e2b360ed9ce04
ubaid5 <60641452+ubaid5@users.noreply.github.com>
2022-10-26 11:06:42 +0500

    BMI Implementation

--- lib/screens/my_activity/bmi_popup.dart
+++ lib/screens/my_activity/bmi_popup.dart
@@ -1,17 +1,15 @@
+import 'package:fitness_app/constants/colors.dart';
+import 'package:fitness_app/widgets/color_remover.dart';
 import 'package:flutter/material.dart';
 
 class BMIPopup extends StatefulWidget {
-  const BMIPopup({Key? key}) : super(key: key);
 
   @override
   State<BMIPopup> createState() => _BMIPopupState();
 }
 
 class _BMIPopupState extends State<BMIPopup> {
-  // the controller for the text field associated with "height"
   final _heightController = TextEditingController();
-
-  // the controller for the text field associated with "weight"
   final _weightController = TextEditingController();
 
   double? _bmi;
@@ -48,54 +46,155 @@ class _BMIPopupState extends State<BMIPopup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.deepOrange,
-        body: Center(
-          child: SizedBox(
-            width: 320,
-            child: Card(
-              color: Colors.white,
-              elevation: 10,
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextField(
-                      keyboardType:
-                      const TextInputType.numberWithOptions(decimal: true),
-                      decoration:
-                      const InputDecoration(labelText: 'Height (m)'),
-                      controller: _heightController,
+        body: ColorRemover(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.04, left: MediaQuery.of(context).size.width*0.075),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                      "Weight",
+                      style: TextStyle(fontSize: 22),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: MediaQuery.of(context).size.width*0.075),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width*0.4,
+                          child: TextField(
+                            keyboardType:
+                            const TextInputType.numberWithOptions(decimal: true),
+                            decoration: const InputDecoration(
+                              // labelText: "Weight",
+                              labelStyle: TextStyle(color: Colors.white),
+                            ),
+                            controller: _weightController,
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                            ),
+                          ),
+                          child: Text(
+                            "KG",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                            ),
+                          ),
+                          child: Text(
+                            "LB",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ),
+                      ],
                     ),
-                    TextField(
-                      keyboardType:
-                      const TextInputType.numberWithOptions(decimal: true),
-                      decoration: const InputDecoration(
-                        labelText: 'Weight (kg)',
-                      ),
-                      controller: _weightController,
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height*0.05),
+                  Text(
+                    "Height",
+                    style: TextStyle(fontSize: 22),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: MediaQuery.of(context).size.width*0.075),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width*0.4,
+                          child: TextField(
+                            keyboardType:
+                            const TextInputType.numberWithOptions(decimal: true),
+                            decoration: const InputDecoration(
+                              // labelText: "Height",
+                              labelStyle: TextStyle(color: Colors.white),
+                            ),
+                            controller: _heightController,
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                            ),
+                          ),
+                          child: Text(
+                            "CM",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                            ),
+                          ),
+                          child: Text(
+                            "IN",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ),
+                      ],
                     ),
-                    ElevatedButton(
-                      onPressed: _calculate,
-                      child: const Text('Calculate'),
+                  ),
+                  // ElevatedButton(
+                  //   onPressed: _calculate,
+                  //   child: const Text('Calculate'),
+                  // ),
+                  // const SizedBox(
+                  //   height: 30,
+                  // ),
+                  // Text(
+                  //   _bmi == null ? 'No Result' : _bmi!.toStringAsFixed(2),
+                  //   style: const TextStyle(fontSize: 50),
+                  //   textAlign: TextAlign.center,
+                  // ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20, right: 35),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        InkWell(
+                          onTap: (){
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "CANCEL",
+                            style: TextStyle(color: Color(0xff1ce5c1), fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        SizedBox(width: MediaQuery.of(context).size.width*0.1),
+                        InkWell(
+                          onTap: (){
+                            _calculate();
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "SAVE",
+                            style: TextStyle(color: Color(0xff1ce5c1), fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Text(
-                      _bmi == null ? 'No Result' : _bmi!.toStringAsFixed(2),
-                      style: const TextStyle(fontSize: 50),
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      _message,
-                      textAlign: TextAlign.center,
-                    )
-                  ],
-                ),
+                  ),
+                  // Text(
+                  //   _message,
+                  //   textAlign: TextAlign.center,
+                  // )
+                ],
               ),
             ),
           ),
